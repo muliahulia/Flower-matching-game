67e6fdcc25a89472ab95c57ea448f3e50d1b86f2
Julia <juliasoaressmith@gmail.com>
2023-10-30 13:15:11 -0700

    Initial Commit

---
+++ oct30game/oct30gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  oct30gameApp.swift
+//  oct30game
+//
+//  Created by Julia Smith on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct oct30gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
